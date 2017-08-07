39730793d84756bf98af6c60cae32aeebacb800f
Jeremy Sparkman <jeremyjames411@gmail.com>
2017-08-07 17:02:20 -0400

    removed script.js moved jquery to controller

--- public/js/home-controller.js
+++ public/js/home-controller.js
@@ -1,5 +1,7 @@
 var app = angular.module('miModule');
 
 app.controller('homeCtrl', function($scope, miFactory) {
-
+  $('img').on('click', function() {
+    $(this).toggleClass('selected');
+  });
 });
--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-
-  $('img').on('click', function () {
-    $(this).toggleClass('selected');
-  });
-
-});
