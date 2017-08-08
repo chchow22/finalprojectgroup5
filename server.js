82dcffd2f2c2d2b440dd84db6dfc85b97ee06fd4
Kevin Naughton <kevinjamesnaughton@gmail.com>
2017-08-08 13:59:57 -0400

    server

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var routes = require('./routes');
+var port = process.env.PORT || 5000;
 
 app.use(bodyParser.json({ extended: true }));
 app.use('/api', routes);
 app.use(express.static(__dirname + '/public'));
 
-app.listen(5000, function() {
+
+
+app.listen(port, function() {
   console.log('Server is running :]');
 });
